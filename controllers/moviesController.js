382390a8fafae3666839766d8d53c1ad9e67ee98
gialli24 <a.gialli2409@gmail.com>
2026-04-20 15:34:50 +0200

    add show and index routes

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,11 +1,37 @@
 const conn = require("../database/db");
 
 const index = (req, res) => {
-    res.send("Movies Index");
+    const sql = "SELECT * FROM movies";
+
+    conn.query(sql, (err, results) => {
+        if (err) return res.status(500).send("Internal Server Error");
+        if (results.length === 0) return res.status(404).send("Not Found");
+
+        res.json(results);
+    });
 };
 
 const show = (req, res) => {
-    res.send("Movies Show");
+    const id = Number(req.params.id);
+
+    const getMovieSql = "SELECT * FROM movies WHERE id = ?";
+
+    conn.query(getMovieSql, [id], (err, results) => {
+        if (err) return res.status(500).send("Internal Server Error");
+        if (results.length === 0) return res.status(404).send("Not Found");
+
+        const movie = results[0];
+
+        const getReviewsSql = "SELECT * FROM reviews WHERE movie_id = ?";
+
+        conn.query(getReviewsSql, [id], (err, results) => {
+            if (err) return res.status(500).send("Internal Server Error");
+
+            movie.reviews = results;
+
+            res.json(movie);
+        })
+    })
 };
 
 module.exports = {
